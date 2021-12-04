310396f7353b8ccea2deef938c10679d19e67874
Bebebobol <lorenzo1711@hotmail.co.uk>
2021-12-03 22:04:11 -0300

    Update views.py

--- mercadinhx/visualizacao2/views.py
+++ mercadinhx/visualizacao2/views.py
@@ -20,25 +20,25 @@ def visualizacao2(request):
     clientes = converter_query(Clientes.objects.all(), retornar="dataframe")
 
     vendas = converter_query(Vendas.objects.all(), retornar="dataframe")
-    # Selecionando os dados
+    
     venda_produtos = converter_query(VendasProdutos.objects.all(), retornar="dataframe")
-
+    # Selecionando os dados
     preco = produtos[['id','preco']]
-    
+    # Renomeando a coluna id
     preco.rename(columns={"id": "id_produto"}, inplace = 1)
-    
+    # Unindo as tabelas venda_produtos e preco
     venda_preco = pd.merge(venda_produtos, preco, how= 'inner', on= 'id_produto')
     # Calculando o total arrecadado por produto
     venda_preco['total_ganho'] = venda_preco['quantidade']*venda_preco['preco']
-    
+    # Agrupando produtos por id
     venda_preco_prod = venda_preco.groupby('id_produto').sum()
-    #Organizando os dados
+    # Criando nova variável id para o gráfico
     venda_preco_prod['id'] = range(len(venda_preco_prod['total_ganho']))
-    
+    # Selecionando dados
     nome_prod = produtos[['nome', 'id']]
-    
+    # Unindo as tabelas venda_produtos_prod e nome_prod
     venda_preco_prod = pd.merge(venda_preco_prod, nome_prod, how= 'inner', on= 'id')
-    #Plotando o gráfico
+    # Plotando o gráfico
     fig = px.bar(venda_preco_prod, x='nome', y='total_ganho')
     
     fig.update_layout(title = 'Receita gerada por cada produto')
